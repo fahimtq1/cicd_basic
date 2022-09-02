997d8af0fbf92dff94df3f290f6a8fbbe45b0e8e
fahimtq1 <fahimqurashi123@outlook.com>
2022-09-02 15:54:52 +0100

    wow

--- app/provision.sh
+++ app/provision.sh
@@ -1,7 +1,5 @@
 sudo apt-get update -y   # best to update and upgrade vm at the start
 sudo apt-get upgrade -y
-sudo apt-get update -y   # best to update and upgrade vm at the start
-sudo apt-get upgrade -y
 sudo apt-get install nginx -y   # starts after installation
 sudo systemctl restart nginx
 sudo systemctl enable nginx
