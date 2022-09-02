c82a0d9795a393a95d0073292a01ec803ced739b
fahimtq1 <fahimqurashi123@outlook.com>
2022-09-02 17:12:53 +0100

    provision edit

--- app/provision.sh
+++ app/provision.sh
@@ -1,8 +1,5 @@
 sudo apt-get update -y   # best to update and upgrade vm at the start
 sudo apt-get upgrade -y
-echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
-sudo apt-get update -y   # best to update and upgrade vm at the start
-sudo apt-get upgrade -y
 sudo apt-get install nginx -y   # starts after installation
 sudo systemctl restart nginx
 sudo systemctl enable nginx
@@ -12,9 +9,8 @@ sudo systemctl restart nodejs
 sudo systemctl enable nodejs
 sudo apt-get install npm -y
 sudo npm install pm2 -g
-sudo cp -f app/default /etc/nginx/sites-available/default
+sudo cp -f jenkins_app/app/default /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 sudo systemctl enable nginx
-cd app/app && npm install
 sudo apt-get update -y   # best to update and upgrade vm at the start
 sudo apt-get upgrade -y
